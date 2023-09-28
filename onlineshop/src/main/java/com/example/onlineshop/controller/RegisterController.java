26286b4b595b4c4253b1369a779280920456acc9
takaya-0808 <takaya1912@gmail.com>
2023-09-28 23:22:31 +0900

    eature/MEM102

--- onlineshop/src/main/java/com/example/onlineshop/controller/RegisterController.java
+++ onlineshop/src/main/java/com/example/onlineshop/controller/RegisterController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -25,6 +26,8 @@ public class RegisterController {
 
     private RegisterValidation registerValidation;
 
+    private RegisterModel registerModel;
+
     @RequestMapping(path = "/register", method = RequestMethod.GET)
     public ModelAndView getLoginForm() {
 
@@ -36,15 +39,28 @@ public class RegisterController {
     }
 
     @RequestMapping(path = "/registerCheck", method = RequestMethod.POST)
-    public ModelAndView checkRegister(@ModelAttribute RegisterModel model) {
-
+    public ModelAndView checkRegister(@ModelAttribute RegisterModel model, @RequestParam(name="button") String name) {
+d
+        if (name.equals("return")) {
+            return new ModelAndView("redirect:/menu");
+        }
+        if (name.equals("clear")) {
+            return new ModelAndView("redirect:/register");
+        }
         var mav = registerValidation.valid(model);        
         if (!mav.isEmpty()) {
             mav.addObject("genderList", sexService.getSexList());
             mav.setViewName("user/register");
             return mav;
         }
+        registerModel = model;
         mav.addObject("registerModel", registerService.checkRegister(model));
+        mav.setViewName("user/registerResult");
+        return mav;
+    }
+
+    @RequestMapping(path = "registerResult", method = RequestMethod.POST)
+    public ModelAndView resultRegister(@RequestParam(name="button") String name) {
 
         // if (registerService.insert(model) == 1) {
         //     mav.addObject("message", "会員登録に成功しました");
@@ -52,9 +68,6 @@ public class RegisterController {
         // } else {
         //     mav.addObject("message", "会員登録に失敗しました");
         // }
-        mav.setViewName("user/registerResult");
-        return mav;
+        return null;
     }
-
-    // @RegisterModel(path = "")
 }
